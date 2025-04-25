50f1c614ae2a96035f40493d78f8b62793940e1e
Railton <railtondantas.code@gmail.com>
2025-04-25 15:33:06 -0300

    expressoes aritmeticas

---
+++ aritmeticas/Main.java
@@ -0,0 +1,16 @@
+package aritmeticas;
+public class Main {
+    public static void main(String[] args){
+        int teste = 10 * 3; 
+        int teste2 = 10 / 5; 
+        int teste3 = 10 - 3;
+        int teste4 = 20 / 5;
+        int resto = 10 % 9;
+        System.out.println(teste);
+        System.out.println(teste2);
+        System.out.println(teste3);
+        System.out.println(teste4);
+        System.out.println(resto);
+    }
+
+}
