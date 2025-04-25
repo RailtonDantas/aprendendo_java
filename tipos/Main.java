7ba1db9ffc870d1bc8be368feef5f5a6e7d42678
Railton <railtondantas.code@gmail.com>
2025-04-25 16:01:28 -0300

    tipos primitivos

---
+++ tipos/Main.java
@@ -0,0 +1,41 @@
+package tipos;
+
+public class Main {
+    public static void main(String[] args){
+        // tipo nome = valor
+
+        int idade = 18;
+        double peso = 1.89;
+        char init = 'F';
+
+        // tipos de valores inteiros, são ele byte,short,int e long
+
+        // byte = inteiros de até 8 bits
+        // short = inteiros de ate 16 bits
+        // int = inteiros de 32 bits
+        // long = inteiros de 64 bits
+
+        byte a = 0;
+        short b = 0;
+        int c = 0;
+        long d = 0;
+
+        // TIPOS DE VALORES DE PONTO FLUTUANTES
+
+        // float são os valores de numeros de pontos flutuantes ate 32 bits
+        // double são os valores de numeros de ponto flutuante ate 64 bits
+
+        float f = 1;
+        double e = 9.43;
+
+        // tem o tipo boolean:
+        boolean gh = true;
+        boolean gj = false;
+
+
+
+        
+
+
+    }
+}
