24ff4ed7d8760495ec81bb7b8f8e739edaad5262
Railton <railtondantas.code@gmail.com>
2025-04-29 12:27:25 -0300

    Entrada de dados

---
+++ LeituraDeDados.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class LeituraDeDados {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in); // tem q instanciar um objeto da classe scanner, pra isso, tem que importar o java.util.scanner
+
+        String x ;
+        x = sc.next(); // pra pegar string no input
+        char carac = x.charAt(0); // pra pegar o caractere da string
+
+        System.out.println("Você digitou o caractere " + carac); 
+        System.out.println("Você digitou a palavra " + x); 
+        double y ;
+        y = sc.nextDouble(); // pra pegar double no input
+        System.out.println("Vc digitou um double " + y);
+  
+        int a;
+        a = sc.nextInt();
+        System.out.println("Voce printou um inteiro " + a);
+
+        // // PEGAR VARIOS INPUTS NA MESMA LINHA
+        String ab;
+        int ac;
+        double ad;
+
+        ab = sc.next();
+        ac = sc.nextInt();
+        ad = sc.nextDouble();
+
+        System.out.println(ab);
+        System.out.println(ac);
+        System.out.println(ad);
+
+
+        // Como ler uma linha inteira
+
+        String linhaAt;
+        String linhaAt2;
+        String linhaAt3;
+        sc.nextLine(); // pra consumir a quebra de linha pendente
+
+        linhaAt = sc.nextLine();
+        linhaAt2 = sc.nextLine();
+        linhaAt3 = sc.nextLine();
+
+        System.out.println(linhaAt);
+        System.out.println(linhaAt2);
+        System.out.println(linhaAt3);
+
+        sc.close();
+    }
+}
---
+++ cast.java
@@ -0,0 +1,26 @@
+
+public class cast {
+    public static void main(String[] args){
+      int x,y;
+      double resultado = 0.0;
+      x = 5;
+      y = 2;
+
+      resultado = x / y; // aqui, como é uma divisão de 2 numeros inteiros, ele vai querer retornar numeros inteiros, logo, isso
+      // retorna 2
+      // para resolver isso, basta declarar x,y como double, ou fazer o casting:
+      resultado = (double) x / y; // isso é o casting, a conversão explicita de tipos
+      System.out.println(resultado);
+
+      // EXEMPLO
+      double a = 5.0;
+      int b = 2;
+
+      b = (int) a; // da erro pois b é int e a é double, logo, não da pr aconverter int para double dessa forma
+      //tem que fazer o casting
+      System.out.println(a);
+      System.out.println(b);
+      
+      
+    }
+}
