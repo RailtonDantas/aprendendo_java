4d24219e4c03fbe29b1788a3c8fdd1b53fb4ba2b
Railton <railtondantas.code@gmail.com>
2025-04-30 15:12:06 -0300

    finalização do básico

---
+++ Bitwise.java
@@ -0,0 +1,20 @@
+public class Bitwise {
+    public static void main(String[] args){
+        // & -> é o and
+        // ele compara bit a bit, por exemplo, se ele vai comparar o 10 em decimal com o 2 em decimal, essas são as etapas
+        // 10(2) =  00001010
+        // 2(2) =   00000010
+        // 10 & 2 = 00000010
+
+        // compara primeiro (0 and 0) -> 0
+        // 1 and 1 -> 1
+        // 1 and 0 -> 0
+        // e por ai vai
+        int t1 = 0;
+        int t2 = 255;
+        System.out.println(t1 ^ t2);
+
+        // o | é o "or", funciona da mesma forma, comparando os bits, porém, como um or
+        // o ^ é o xor, também funciona igual, porém, compara como um xor
+    }
+}
---
+++ Funcoes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Funcoes{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int maior = meuMax(a,b,c);
+        mostrarNaTela(maior);
+
+        sc.close();
+    }
+
+    public static int meuMax(int a,int b,int c){
+        if (a > b && a > c) {
+            return a;
+        }
+        else if (b > c) {
+            return b;
+        }
+        else{
+            return c;
+        }
+    }
+
+    public static void mostrarNaTela(int maior){
+        System.out.println("O maior número é " + maior);
+    }
+}
